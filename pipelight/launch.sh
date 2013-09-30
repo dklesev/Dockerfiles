898aa3406dcb335b83a61a417e9a054877d07813
Thermionix <bond711@gmail.com>
2013-09-30 14:29:34 +1000

    new file:   apcupsd/Dockerfile 	modified:   pipelight/Dockerfile 	modified:   pipelight/launch.sh 	new file:   pipelight/pipelight-docker

--- pipelight/launch.sh
+++ pipelight/launch.sh
@@ -8,13 +8,10 @@ DOCKER_PASSWORD=`pwgen -c -n -1 12`
 DOCKER_ENCRYPYTED_PASSWORD=`perl -e 'print crypt('"$DOCKER_PASSWORD"', "aa"),"\n"'`
 useradd -M -d /home/docker -p $DOCKER_ENCRYPYTED_PASSWORD docker
 sed -Ei 's/adm:x:4:/docker:x:4:docker/' /etc/group
-#adduser docker sudo
+#adduser docker wheel
 
-# Set the default shell as bash for docker user.
 chsh -s /bin/bash docker
-
-#Set all the files and subdirectories from /home/docker with docker permissions.
-chown -R docker:docker /home/docker/*
+chown -R docker:docker /home/docker
 
 export SSH_AUTH_SOCK=/dev/null
 export SSH_ASKPASS=/bin/false
@@ -23,32 +20,19 @@ echo "starting xdm"
 /etc/init.d/xdm start
 
 echo "starting sshd"
-/usr/sbin/sshd
+/usr/sbin/sshd &
 
-echo "#########################################"
-echo "Run the following to connect to firefox: "
-#echo ssh -oStrictHostKeyChecking=no -oCheckHostIP=no -YC -c blowfish docker@localhost -p $PORT ./
-echo User: docker Password: $DOCKER_PASSWORD
-echo "#########################################"
-
-DISPLAY=":0"
+DISPLAY=":8"
 export DISPLAY 
-#export SSH_AUTH_SOCK=/dev/null
-#export SSH_ASKPASS=/bin/false
 
-# update default useragent string?
+echo "#########################################"
+echo "sshpass: $DOCKER_PASSWORD"
+echo "display: $DISPLAY"
+echo "#########################################"
 
-echo "starting xpra session"
-#sudo -u docker 
-#xpra start $DISPLAY & sleep 5 ; DISPLAY=$DISPLAY firefox
-#sudo -u docker xpra start :$DISPLAY --start-child=firefox
-xpra start :$DISPLAY
-#&
+#Prefs.js
+#user_pref("general.useragent.override","Some string");
+#Mozilla/5.0 (Windows NT 6.1; WOW64; rv:22.0) Gecko/20100101 Firefox/22.0
 
-#echo "attaching to xpra session"
-#xpra attach $DISPLAY
+su -c "xpra start $DISPLAY --start-child=firefox --no-daemon" docker
 
-
-### firefox.sh
-# xpra attach ssh:user@server$DISPLAY
-# DISPLAY=$DISPLAY firefox
